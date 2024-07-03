ddaf0d97fe2b0e52d98220dea05d82517d6e663f
stevebstorm <steve.lorent@gmail.Com>
2024-07-03 12:31:45 +0200

    Ajoutez des fichiers projet.

---
+++ ApiSample/Controllers/FilmController.cs
@@ -0,0 +1,24 @@
+﻿using ApiSample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController(FilmService _filmService) : ControllerBase
+    {
+        //private readonly FilmService _filmService;
+
+        //public FilmController(FilmService filmService)
+        //{
+        //    _filmService = filmService;
+        //}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_filmService.Liste);
+        }
+    }
+}
---
+++ ApiSample/Services/FilmService.cs
@@ -0,0 +1,32 @@
+﻿using ApiSample.Models;
+
+namespace ApiSample.Services
+{
+    public class FilmService
+    {
+        public List<Film> Liste { get; set; }
+        public FilmService()
+        {
+            Liste = new List<Film>();
+            Liste.Add(new Film
+            {
+                Id = 1,
+                Titre = "Star Wars",
+                Synopsis = "L'histoire d'un pirate et d'une boule de poil",
+                AnneeSortie = 1977, Bonfilm = true, Genre = "Sci-fi",
+                Realisateur = new Personne { Nom = "Lucas", Prenom = "George"}
+            });
+        }
+
+        public void Add(Film film)
+        {
+            film.Id = Liste.Max(x => x.Id) + 1;
+            Liste.Add(film);
+        }
+
+        public Film GetById(int id)
+        {
+            return Liste.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
